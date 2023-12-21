eb0b3e4c1dc7e90f47c561a70cc3adf25ebe08ac
Caio Gomes <ocaioluis@gmail.com>
2023-12-21 20:23:23 +0000

    test oicd config fetch
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/core/oicd_config.rs
+++ src/core/oicd_config.rs
@@ -1,4 +1,5 @@
 use reqwest::Client;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct OidcConfigResponse {
@@ -19,4 +20,22 @@ async fn fetch_oidc_config(
     let config: OidcConfigResponse = response.json().await?;
 
     Ok(config)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_fetch_oidc_config() {
+        let client = reqwest::Client::new();
+
+        let res = fetch_oidc_config(
+            &client,
+            "http://localhost:3001/oidc/.well-known/openid-configuration",
+        )
+        .await;
+
+        assert!(res.is_ok())
+    }
 }
