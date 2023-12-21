013997da6785bcf0d8ffcfc22b381ab4ef99376f
Caio Gomes <ocaioluis@gmail.com>
2023-12-21 18:32:40 +0000

    define oicd response type
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/core/oicd_config.rs
+++ src/core/oicd_config.rs
@@ -1,5 +1,15 @@
 use reqwest::Client;
 
+#[derive(Debug, Deserialize)]
+struct OidcConfigResponse {
+    authorization_endpoint: String,
+    token_endpoint: String,
+    end_session_endpoint: String,
+    revocation_endpoint: String,
+    jwks_uri: String,
+    issuer: String,
+}
+
 async fn fetch_oidc_config(
     client: &Client,
     endpoint: &str,
