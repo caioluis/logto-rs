82baa7753116a6a5acc5b170f5fae9f58f5b7723
Caio Gomes <ocaioluis@gmail.com>
2023-12-23 03:08:01 +0000

    test(signin): write first test on sign in function
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/core/sign_in.rs
+++ src/core/sign_in.rs
@@ -1,13 +1,45 @@
+use std::collections::HashSet;
+
 use reqwest::Url;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct SignInUriGenerationOptions {
+pub struct SignInUriGenerationOptions {
     authorization_endpoint: String,
     client_id: String,
     redirect_uri: String,
     code_challenge: String,
     state: String,
+    scopes: Option<Vec<String>>,
+    resources: Option<Vec<String>>,
+    prompt: Option<String>,
+}
+
+enum ReservedScopes {
+    OfflineAccess,
+    OpenId,
+}
+
+impl ReservedScopes {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::OfflineAccess => "offline_access",
+            Self::OpenId => "openid",
+        }
+    }
+}
+
+fn with_default_scopes(mut scopes: Vec<String>) -> Vec<String> {
+    let default_scopes: Vec<String> = vec![
+        ReservedScopes::OpenId.as_str().to_owned(),
+        ReservedScopes::OfflineAccess.as_str().to_owned(),
+    ];
+
+    scopes.extend(default_scopes);
+    scopes.sort_unstable();
+    scopes.dedup();
+
+    return scopes;
 }
 
 const CODE_CHALLENGE_METHOD: &str = "S256";
@@ -18,13 +50,65 @@ pub fn generate_signin_uri(
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut url = Url::parse(&options.authorization_endpoint)?;
 
+    let mut scopes_set = HashSet::<String>::new();
+    if let Some(scopes) = options.scopes {
+        let scopes_with_default = with_default_scopes(scopes);
+        for scope in scopes_with_default {
+            scopes_set.insert(scope);
+        }
+    }
+
+    let mut resources = Vec::new();
+    if options.resources.is_some() {
+        for resource in options.resources.unwrap() {
+            if !resources.contains(&resource) {
+                resources.push(resource);
+            }
+        }
+    }
+
     url.query_pairs_mut()
         .append_pair("client_id", &options.client_id)
         .append_pair("redirect_uri", &options.redirect_uri)
         .append_pair("code_challenge", &options.code_challenge)
         .append_pair("code_challenge_method", CODE_CHALLENGE_METHOD)
         .append_pair("state", &options.state)
-        .append_pair("response_type", RESPONSE_TYPE);
+        .append_pair("response_type", RESPONSE_TYPE)
+        .append_pair(
+            "scopes",
+            scopes_set
+                .into_iter()
+                .collect::<Vec<String>>()
+                .join(" ")
+                .as_str(),
+        )
+        .append_pair("resource", resources.join(" ").as_str());
 
     Ok(url.as_str().to_owned())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_generate_signin_uri() {
+        let generated_uri = generate_signin_uri(SignInUriGenerationOptions {
+            authorization_endpoint: "http://localhost:3001/oidc/sign-in".to_string(),
+            client_id: "clientId".to_string(),
+            redirect_uri: "https://example.com/callback".to_string(),
+            code_challenge: "codeChallenge".to_string(),
+            state: "state".to_string(),
+            scopes: None,
+            resources: None,
+            prompt: None,
+        });
+
+        if let Ok(uri) = generated_uri {
+            assert_eq!(
+                uri,
+                "http://localhost:3001/oidc/sign-in?client_id=clientId&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&code_challenge=codeChallenge&code_challenge_method=S256&state=state&response_type=code&prompt=consent&scope=openid+offline_access+profile"
+            )
+        }
+    }
 }
