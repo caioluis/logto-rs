e5ac5293efd5e065a0138f42233712f77a7352cd
Caio Gomes <ocaioluis@gmail.com>
2023-12-25 07:47:46 +0000

    test(generators): random codes
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/utils/generators.rs
+++ src/utils/generators.rs
@@ -22,3 +22,16 @@ fn generate_random_string() -> String {
             .collect::<String>(),
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn random_strings() {
+        let first_code = generate_code_verifier();
+        let second_code = generate_code_verifier();
+
+        assert_ne!(first_code, second_code)
+    }
+}
