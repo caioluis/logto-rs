36a0238daa960d6fcbd30cc04b4a22a5ff5faaac
Caio Gomes <ocaioluis@gmail.com>
2023-12-27 20:33:19 +0000

    test(verify_uri): add more test cases
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/utils/verify_and_parse_code_from_callback_uri.rs
+++ src/utils/verify_and_parse_code_from_callback_uri.rs
@@ -11,8 +11,11 @@ struct UnverifiedUris {
 fn verify_and_parse_code_from_callback_uri(
     params: UnverifiedUris,
 ) -> Result<String, Box<dyn Error>> {
-    if params.callback_uri.starts_with(&params.redirect_uri) {
-        return Err("malformed Uri".into());
+    if !params.callback_uri.starts_with(&params.redirect_uri) {
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Callback URI does not start with redirect URI",
+        )));
     }
 
     let parsed_uri = Url::parse(&params.callback_uri)?;
@@ -46,13 +49,105 @@ mod tests {
     fn test_verify_and_parse_code_from_callback_uri() -> Result<()> {
         let params = UnverifiedUris {
             callback_uri: "http://example.com/callback?state=123456&code=abcdef".to_string(),
-            redirect_uri: "http://example.com/redirect".to_string(),
+            redirect_uri: "http://example.com/callback".to_string(),
             state: "123456".to_string(),
         };
 
         let result = verify_and_parse_code_from_callback_uri(params).unwrap_or_default();
 
         assert_eq!(result, "abcdef");
+
+        Ok(())
+    }
+
+    #[test]
+    fn wrong_uri_test_verify_and_parse_code_from_callback_uri() {
+        let params = UnverifiedUris {
+            callback_uri: "http://example.com/callback?state=123456&code=abcdef".to_string(),
+            redirect_uri: "http://example.com/redirect".to_string(),
+            state: "123456".to_string(),
+        };
+
+        let result = verify_and_parse_code_from_callback_uri(params);
+
+        match result {
+            Ok(_) => panic!("Expected error but got ok"),
+            Err(e) => assert_eq!(
+                &*e.to_string(),
+                "Callback URI does not start with redirect URI"
+            ),
+        }
+    }
+
+    #[test]
+    fn callback_uri_with_error() -> Result<()> {
+        let params = UnverifiedUris {
+            callback_uri: "http://example.com/callback?state=123456&code=abcdef&error=some_error"
+                .to_string(),
+            redirect_uri: "http://example.com/callback".to_string(),
+            state: "123456".to_string(),
+        };
+
+        let result = verify_and_parse_code_from_callback_uri(params);
+
+        match result {
+            Ok(_) => panic!("Expected error but got ok"),
+            Err(e) => assert_eq!(&*e.to_string(), "uri contains error"),
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn callback_uri_without_state() -> Result<()> {
+        let params = UnverifiedUris {
+            callback_uri: "http://example.com/callback?code=abcdef".to_string(),
+            redirect_uri: "http://example.com/callback".to_string(),
+            state: "123456".to_string(),
+        };
+
+        let result = verify_and_parse_code_from_callback_uri(params);
+
+        match result {
+            Ok(_) => panic!("Expected error but got ok"),
+            Err(e) => assert_eq!(&*e.to_string(), "states don't match"),
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn callback_uri_with_mismatched_state() -> Result<()> {
+        let params = UnverifiedUris {
+            callback_uri: "http://example.com/callback?state=wrong&code=abcdef".to_string(),
+            redirect_uri: "http://example.com/callback".to_string(),
+            state: "123456".to_string(),
+        };
+
+        let result = verify_and_parse_code_from_callback_uri(params);
+
+        match result {
+            Ok(_) => panic!("Expected error but got ok"),
+            Err(e) => assert_eq!(&*e.to_string(), "states don't match"),
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn callback_uri_without_code() -> Result<()> {
+        let params = UnverifiedUris {
+            callback_uri: "http://example.com/callback?state=123456".to_string(),
+            redirect_uri: "http://example.com/callback".to_string(),
+            state: "123456".to_string(),
+        };
+
+        let result = verify_and_parse_code_from_callback_uri(params);
+
+        match result {
+            Ok(_) => panic!("Expected error but got ok"),
+            Err(e) => assert_eq!(&*e.to_string(), "code parameter is missing"),
+        }
 
         Ok(())
     }
